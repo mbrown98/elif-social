46ae5c173aae3ab76176c673085c99d32754097a
Matt Brown <matthewbrown0798@gmail.com>
2021-01-04 16:29:17 -0500

    delete post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,4 +71,30 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+//@route DELETE api/posts/:id
+//@description Delete post by id
+//@access Private
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check if post exists
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    //check user deleting
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+    res.json({ msg: "Post deleted" });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
