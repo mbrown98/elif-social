7a4aa4fbc40415961dd0a08e29ba574f1087e458
Matt Brown <matthewbrown0798@gmail.com>
2021-01-06 13:25:07 -0500

    add experience forms

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+} from "../actions/types";
 
 const initialState = {
   profile: null,
@@ -13,6 +18,7 @@ export default function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return { ...state, profile: payload, loading: false };
     case PROFILE_ERROR:
       return { ...state, error: payload, loading: false };
