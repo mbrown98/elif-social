8aa2e033c5315ad7ad5e65130f0fc6b664f3b081
Matt Brown <matthewbrown0798@gmail.com>
2021-01-06 09:52:26 -0500

    adding logged in user profile to redux

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -13,3 +13,8 @@ export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 
 export const LOGOUT = "LOGOUT";
+
+// PROFILE
+
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
