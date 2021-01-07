0267a867a19642eb84b6d28b7a9bff0bb25fc519
Matt Brown <matthewbrown0798@gmail.com>
2021-01-07 14:38:03 -0500

    get post action and reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -22,5 +22,9 @@ export const CLEAR_PROFILE = "CLEAR_PROFILE";
 export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
 
+// POSTS
+export const GET_POSTS = "GET_POSTS";
+export const POST_ERROR = "POST_ERROR";
+
 // Github Repos
 export const GET_REPOS = "GET_REPOS";
