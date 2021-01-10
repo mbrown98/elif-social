dd52c70fdbaf1c330801e900dc79492fd3d2372f
Matt Brown <matthewbrown0798@gmail.com>
2021-01-10 18:18:48 -0500

    enabled cors on server and chnaged front end axios default url

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,12 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
+import axios from "axios";
 
 import "./App.css";
 // import { compareSync } from "bcryptjs";
+
+axios.defaults.baseURL = "https://laces-be.herokuapp.com";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
